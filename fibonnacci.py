1cfc413d4ecd6458a49a6b561f765e8567d784d8
Grobik228 <lednikoviperiod@tutamail>
2024-10-18 19:14:21 +0300

    main3

--- fibonnacci.py
+++ fibonnacci.py
@@ -1,9 +1,18 @@
-
-fib1 = fib2 = 1
-n = int(input('Введите число:'))
-print(0,fib1, fib2, end=' ')
-
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end=' ')
-
+input_data = open('input.txt', 'r') #открываем файлы
+output_data = open('output.txt', 'w')
+n = input_data.readline()
+# Ввод первых  значений последовательности
+a = 0
+b = 1
+# Вывод 2 значений последовательности через запятую
+output_data.write(str(a))
+output_data.write(", ")
+output_data.write(str(b))
+output_data.write(", ")
+for i in range(2, int(n) + 1): # Входим в цикл
+    a, b = b, a + b # Выводим в файл новое значение переменной b
+    output_data.write(str(b))
+    if i != (int(n)):
+     output_data.write(", ")
+input_data.close() #закрываем файлы
+output_data.close()
